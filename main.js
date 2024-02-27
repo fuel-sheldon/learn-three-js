bb72888596607727e4d7e031439ea4c52ffb7443
SheldonFam <sheldon.fam@fuel.com.my>
2024-02-27 17:39:53 +0800

    initial-commit

---
+++ main.js
@@ -0,0 +1,172 @@
+// import * as THREE from "three";
+// import "./style.css";
+// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+// import * as dat from "dat.gui";
+// import { gsap } from "gsap";
+
+// Set up scene
+// const scene = new THREE.Scene();
+
+// // Create sphere
+// const geometry = new THREE.SphereGeometry(3, 64, 64);
+// const material = new THREE.MeshBasicMaterial({
+//   color: "#FFA500",
+// });
+// const sphere = new THREE.Mesh(geometry, material);
+// scene.add(sphere);
+
+// //Sizes
+// const sizes = {
+//   width: window.innerWidth,
+//   height: window.innerHeight,
+// };
+
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   sizes.width / sizes.height,
+//   0.1,
+//   1000
+// );
+// // camera.position.z = 20;
+// camera.position.set(0, 0, 20);
+// scene.add(camera);
+
+// //Renderer
+
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(sizes.width, sizes.height);
+// renderer.render(scene, camera);
+// document.body.appendChild(renderer.domElement);
+
+// // Position cube
+// sphere.position.z = -10;
+
+// // Add rotation animation
+// function animate() {
+//   requestAnimationFrame(animate);
+//   renderer.render(scene, camera);
+// }
+
+// animate();
+
+// // gsap.to(sphere.rotation, {
+// //   duration: 5,
+// //   x: 10,
+// //   repeat: -1,
+// // });
+
+// gsap.to(sphere.position, {
+//   duration: 10,
+//   x: 50,
+//   repeat: -1,
+//   ease: "back.inOut",
+// });
+
+// // Resize
+// window.addEventListener("resize", () => {
+//   //update sizes
+//   sizes.width = window.innerHeight;
+//   sizes.height = window.innerHeight;
+
+//   //Update camera
+//   camera.aspect = sizes.width / sizes.height;
+//   camera.updateProjectionMatrix();
+//   renderer.setSize(sizes.width, sizes.height);
+// });
+
+import "./style.css";
+import * as THREE from "three";
+import gsap from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+//Scene
+const scene = new THREE.Scene();
+
+//Create our sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: "#FFA500",
+  roughness: 0.2,
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+//Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+//Light
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 10, 20);
+scene.add(light);
+
+//Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+camera.position.z = 10;
+scene.add(camera);
+
+//Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGL1Renderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2);
+renderer.render(scene, camera);
+
+//Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 20;
+
+//Resize
+window.addEventListener("resize", () => {
+  //update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  //update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const loop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+loop();
+
+//timeline
+const t1 = gsap.timeline({ defaults: { duration: 1 } });
+t1.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+
+//mouse animation
+let mouseDown = false;
+let rgb = [];
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mouseup", () => (mouseDown = false));
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ];
+
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    });
+  }
+});
