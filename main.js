80ce8d576903b428abe850391d1ca1659e3f6a3e
SheldonFam <sheldon.fam@fuel.com.my>
2024-03-08 13:46:38 +0800

    fix the rotate animation

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ const camera = new THREE.PerspectiveCamera(
   5, //fov
   sizes.width / sizes.height, //aspect
   0.1, // near
-  500 // far
+  1000 // far
 );
 camera.position.z = 20;
 scene.add(camera);
@@ -106,7 +106,6 @@ window.addEventListener("resize", () => {
   renderer.setSize(sizes.width, sizes.height);
 });
 
-let angle = 0;
 const loop = () => {
   // controls.update();
   renderer.render(scene, camera);
@@ -114,8 +113,25 @@ const loop = () => {
 };
 loop();
 
+let pivot = new THREE.Object3D();
+// pivot.position.set(0, 0, 0);
+pivot.add(mesh2, mesh3, mesh4, mesh5);
+scene.add(pivot);
+
+// mesh2.position.x = 0;
+// mesh2.position.y = 20;
+
+// mesh3.position.x = 0;
+// mesh3.position.y = 20;
+
+// mesh4.position.x = 0;
+// mesh4.position.y = 20;
+
+// mesh5.position.x = 0;
+// mesh5.position.y = 20;
+
 //timeline
-const t1 = gsap.timeline({ defaults: { duration: 20 } });
+const t1 = gsap.timeline({ defaults: { duration: 5 } });
 //from far to near
 // t1.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
 // t1.fromTo(
@@ -123,12 +139,6 @@ const t1 = gsap.timeline({ defaults: { duration: 20 } });
 //   { z: 0, x: 0, y: 0 },
 //   { z: 1, x: 1, y: 1, ease: "bounce.out" }
 // );
-
-//bounce effect
-// t1.fromTo(mesh.position, { y: 0 }, { y: -10, ease: "bounce.out" });
-
-// First, set the initial position of the sphere in front
-// t1.fromTo(mesh.position, { z: -20 }, { z: 0 });
 
 // Then, animate the sphere to move to its final position at the back
 t1.to(
@@ -139,53 +149,55 @@ t1.to(
     mesh4.position,
     mesh5.position,
   ],
-  { z: -350 }
+  { z: -300 }
 );
 
+t1.to(pivot.rotation, { duration: 5, z: -10 });
+
 // After the spheres reach their final position, start the rotation
-t1.add(() => {
-  let angle = 0;
-  const rotateSpheres = () => {
-    angle += 0.01;
-    const radius = 10;
+// t1.add(() => {
+//   let angle = 0;
+//   const rotateSpheres = () => {
+//     angle += 0.01;
+//     const radius = 10;
 
-    // Update positions of all spheres based on angle
-    const x = radius * Math.cos(angle);
-    const z = radius * Math.sin(angle);
-    mesh.position.x = x;
-    mesh.position.z = z;
+//     // Update positions of all spheres based on angle
+//     // const x = radius * Math.cos(angle);
+//     const z = radius * Math.sin(angle);
+//     // mesh.position.x = x;
+//     mesh.position.z = z;
 
-    mesh2.position.x = -x;
-    mesh2.position.z = -z;
+//     // mesh2.position.x = -x;
+//     mesh2.position.z = -z;
 
-    mesh3.position.x = -z;
-    mesh3.position.z = x;
+//     // mesh3.position.x = -z;
+//     mesh3.position.z = x;
 
-    mesh4.position.x = z;
-    mesh4.position.z = -x;
+//     // mesh4.position.x = z;
+//     mesh4.position.z = -x;
 
-    mesh5.position.x = -z;
-    mesh5.position.z = -x;
+//     // mesh5.position.x = -z;
+//     mesh5.position.z = -x;
 
-    // Rotate all spheres around their own axis
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
-    mesh2.rotation.x += 0.01;
-    mesh2.rotation.y += 0.01;
-    mesh3.rotation.x += 0.01;
-    mesh3.rotation.y += 0.01;
-    mesh4.rotation.x += 0.01;
-    mesh4.rotation.y += 0.01;
-    mesh5.rotation.x += 0.01;
-    mesh5.rotation.y += 0.01;
+//     // Rotate all spheres around their own axis
+//     // mesh.rotation.x += 0.01;
+//     mesh.rotation.z += 0.01;
+//     // mesh2.rotation.x += 0.01;
+//     mesh2.rotation.z += 0.01;
+//     // mesh3.rotation.x += 0.01;
+//     mesh3.rotation.z += 0.01;
+//     // mesh4.rotation.x += 0.01;
+//     mesh4.rotation.z += 0.01;
+//     // mesh5.rotation.x += 0.01;
+//     mesh5.rotation.z += 0.01;
 
-    // Call the function recursively
-    requestAnimationFrame(rotateSpheres);
-  };
+//     // Call the function recursively
+//     requestAnimationFrame(rotateSpheres);
+//   };
 
-  // Start the rotation
-  rotateSpheres();
-});
+//   // Start the rotation
+//   rotateSpheres();
+// });
 // t1.fromTo(mesh2.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
 // t1.fromTo(mesh3.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
 // t1.fromTo(mesh4.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
@@ -213,3 +225,5 @@ t1.add(() => {
 //     });
 //   }
 // });
+
+console.log(mesh, mesh2, mesh3, mesh4, mesh5);
